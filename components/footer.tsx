d6d8495c0c1532574745e5647359e0c75bdbe817
Sambego <sambellen@gmail.com>
2023-10-10 11:16:08 +0200

    Update footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,11 +4,11 @@ import Container from './container';
 
 const Footer: FC = () => {
     return (
-        <header className={styles.header}>
+        <footer className={styles.footer}>
             <Container>
                 <p>Made with ❤️ by <a href="https://twitter.com/sambego" target="_blank" rel="noreferrer"><strong>@sambego</strong></a> | <a href="https://github.com/Sambego/dpop-playground" target="_blank" rel="noreferrer">Source</a></p>
             </Container>
-        </header>        
+        </footer>        
     )
 }
 
