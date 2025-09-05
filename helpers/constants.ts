8d115c491713e4e0acfabf7f985f60717b935f56
Sambego <sambellen@gmail.com>
2025-09-05 15:29:34 +0200

    Cleanup old code

--- helpers/constants.ts
+++
@@ -1,4 +0,0 @@
-export const AUTHORIZATION_SERVER: string = 'https://auth.example.com';
-export const API_SERVER: string = 'https://api.example.com';
-export const APP_DOMAIN: string = 'https://app.example.com';
-export const USER: string = 'Jane Doe';
